200d561af279bd1b04154208682687c82761d11c
code2nvim <code2nvim@gmail.com>
2024-09-06 00:08:12 +0800

    GameState: add Menu

--- src/sid/state.rs
+++ src/sid/state.rs
@@ -4,9 +4,10 @@ pub struct PlayingPlugin;
 
 impl Plugin for PlayingPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<Cursor>()
-            .init_state::<GameState>()
+        app.init_state::<GameState>()
+            .init_resource::<Cursor>()
             .add_systems(Startup, (spawn_camera, spawn_ground, spawn_light))
+            .add_systems(Update, press_to_start)
             .add_systems(OnEnter(GameState::Playing), spawn_player)
             .add_systems(
                 Update,
@@ -22,5 +23,15 @@ impl Plugin for PlayingPlugin {
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Default, States)]
 pub enum GameState {
     #[default]
+    Menu,
     Playing,
+}
+
+pub fn press_to_start(
+    mut game_state: ResMut<NextState<GameState>>,
+    kerpress: Res<ButtonInput<KeyCode>>,
+) {
+    if kerpress.pressed(KeyCode::Space) {
+        game_state.set(GameState::Playing);
+    }
 }
