193f68440b55da0dc025bbea6d88e8534fb9a1b9
code2nvim <code2nvim@gmail.com>
2024-09-12 23:00:58 +0800

    logic change: if let Ok(foo) -> let Ok(foo) ... else ...

--- src/sid/player.rs
+++ src/sid/player.rs
@@ -18,38 +18,41 @@ pub fn update_direction(
     mut direction: Query<&mut Movement, With<Player>>,
     keys: Res<ButtonInput<KeyCode>>,
 ) {
-    if let Ok(mut direction) = direction.get_single_mut() {
-        direction.0 = {
-            let mut direction = Vec3::ZERO;
-            if keys.pressed(KeyCode::KeyW) {
-                direction += Vec3::new(0.0, 0.0, 1.0);
-            }
-            if keys.pressed(KeyCode::KeyA) {
-                direction += Vec3::new(1.0, 0.0, 0.0);
-            }
-            if keys.pressed(KeyCode::KeyS) {
-                direction += Vec3::new(0.0, 0.0, -1.0);
-            }
-            if keys.pressed(KeyCode::KeyD) {
-                direction += Vec3::new(-1.0, 0.0, 0.0);
-            }
-            direction
-        };
-    }
+    let Ok(mut direction) = direction.get_single_mut() else {
+        return;
+    };
+    direction.0 = {
+        let mut direction = Vec3::ZERO;
+        if keys.pressed(KeyCode::KeyW) {
+            direction += Vec3::new(0.0, 0.0, 1.0);
+        }
+        if keys.pressed(KeyCode::KeyA) {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+        if keys.pressed(KeyCode::KeyS) {
+            direction += Vec3::new(0.0, 0.0, -1.0);
+        }
+        if keys.pressed(KeyCode::KeyD) {
+            direction += Vec3::new(-1.0, 0.0, 0.0);
+        }
+        direction
+    };
 }
 
 pub fn move_player(mut player: Query<(&mut Transform, &Movement), With<Player>>, time: Res<Time>) {
-    if let Ok((mut transform, direction)) = player.get_single_mut() {
-        let movement = direction.0.normalize_or_zero() * time.delta_seconds() * PLAYER_SPEED;
-        transform.translation += movement;
-    }
+    let Ok((mut transform, direction)) = player.get_single_mut() else {
+        return;
+    };
+    let movement = direction.0.normalize_or_zero() * time.delta_seconds() * PLAYER_SPEED;
+    transform.translation += movement;
 }
 
 pub fn rotate_player(cursor: Res<Cursor>, mut player: Query<&mut Transform, With<Player>>) {
-    if let Ok(mut transform) = player.get_single_mut() {
-        let cursor = cursor.0;
-        let player = transform.translation;
-        let direction = (cursor - player).normalize();
-        transform.rotation = Quat::from_rotation_y(direction.x.atan2(direction.z));
-    }
+    let Ok(mut transform) = player.get_single_mut() else {
+        return;
+    };
+    let cursor = cursor.0;
+    let player = transform.translation;
+    let direction = (cursor - player).normalize();
+    transform.rotation = Quat::from_rotation_y(direction.x.atan2(direction.z));
 }
